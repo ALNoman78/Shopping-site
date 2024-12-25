fc16ab3365b31f0b3044c1a5b6c52e91b3392134
ALNoman78 <abdullaalno078@gmail.com>
2024-12-25 16:23:25 +0600

    dynamic value and cart items added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useLoaderData } from 'react-router-dom'
 import { getCartData } from '../utility/utility'
+import CartDetails from '../CartDetails/CartDetails'
 
 const Cart = () => {
     const allData = useLoaderData();
     const [cart, setCart] = useState([]);
-    console.log(allData);
-    
+    // console.log(allData);
+
     useEffect(() => {
         const storedCartList = getCartData()
         const cartList = storedCartList.map((id) => parseInt(id))
         const cartData = allData.filter((item) => cartList.includes(item.id))
         setCart(cartData)
     }, [])
+
+    // const { cloth_name, image_link, price, review } = items
+
     return (
         <div>
             <h2>Cart : {cart.length}</h2>
+            {
+                cart.map((item , idx) => <CartDetails key={idx} attier={item}></CartDetails>)
+            }
         </div>
     )
 }
---
+++ src/components/CartDetails/CartDetails.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CartDetails = ({attier}) => {
+    const { cloth_name, image_link, price, review } = attier
+    return (
+        <div className='md:m-5 '>
+            <div className="card bg-base-100  shadow-xl md:flex-row flex-col gap-6">
+                <figure className="md:p-10 md:w-[250px] h-auto w-10/12 mx-auto md:m-5 p-5 bg-base-200 ">
+                    <img 
+                        src={image_link}
+                        alt="Shoes"
+                        className="rounded-xl w-44" />
+                </figure>
+                <div className="card-body">
+                    <h2 className='text-4xl font-bold'>{cloth_name}</h2>
+                    <p className='font-medium text-[15px]'>Reviews : {review}</p>
+                    <p className='text-lg font-medium text-[rgba(19,19,19,0.8)]'>Price : {price}</p>
+                    <div className='flex items-center gap-6'>
+                        <button className='btn btn-success rounded-xl text-white'>Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+CartDetails.propTypes = {}
+
+export default CartDetails
